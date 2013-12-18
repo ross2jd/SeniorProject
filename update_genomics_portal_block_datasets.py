ba4c9ffa0b76dd16c5788fd2a7f84c8b9f92419a
Jordan Ross <ross.j101@gmail.com>
2013-12-18 10:33:37 -0500

    Added a file to parse genomics portal website and update the database table with the available datasets

---
+++ update_genomics_portal_block_datasets.py
@@ -0,0 +1,77 @@
+###############################################################################
+## update_genomics_portal_block_datasets.py
+##
+## This script parses the HTML from the Genomics Portal website and takes the
+## dataset names and descriptions and updates the MySQL table,
+## genomics_portal_block_datasets, with the parsed values. This script is meant
+## to be run by a scheduled task on the server.
+##
+## Revision History
+## jross    12/18/2013  Created.
+##
+###############################################################################
+__author__ = 'Jordan Ross'
+
+import MySQLdb
+import urllib2
+from BeautifulSoup import BeautifulSoup
+
+user_agent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'
+headers = { 'User-Agent' : user_agent }
+req = urllib2.Request('http://www.eh3.uc.edu/GenomicsPortals/experiment.do', None, headers)
+response = urllib2.urlopen(req)
+page = response.read()
+soup = BeautifulSoup(''.join(page))
+portal_list = soup.findAll("td","ExperiPortalsTableCells")
+# portal_list contains all the <td> and all content between them that make up the table of available portals.
+# Now the entries with an anchor tag (<a>) will contain the portal names while the remaining entries will contain the
+# description
+portal_name_list = []
+portal_description_list = []
+
+for portal in portal_list:
+    # This will find the portal names and strip away any unwanted characters and append it to the list of portal names
+    if portal.find("a") is not None:
+        for string_port in portal.findAll(text=True):
+            string_stripped = str(string_port).replace('\n', "").replace('\t', "")
+            if string_stripped is not "":
+                portal_name_list.append(string_stripped)
+    else:
+        for string_desc in portal.findAll(text=True):
+            string_stripped = str(string_desc).replace('\n', "").replace('\t', "")
+            if string_stripped is not "":
+                portal_description_list.append(string_stripped)
+
+if len(portal_description_list) is not len(portal_name_list):
+    # We have an error and should throw an exception
+    print "The number of descriptions do not match the number of portal names!"
+
+# Now we have the list of descriptions and the list of portal names that we can add into the database
+# Comment out the db stuff because we cant test on desktop
+db = MySQLdb.connect(host="127.0.0.1", port=3306, user="root", passwd="Hockey101", db="webbioblocks")
+cursor = db.cursor()
+sql = "TRUNCATE TABLE genomics_portal_block_datasets"
+# We are going to be very simple and just delete all the entries in the table and re add them.
+try:
+    # Execute the SQL command
+    cursor.execute(sql)
+except:
+    print "Error: unable to delete the table entries"
+
+# Now repopulate the table
+for i in range(0, len(portal_name_list)):
+    sql = "INSERT INTO genomics_portal_block_datasets (portal_name, description) VALUES('%s', '%s')" % \
+          (portal_name_list[i], portal_description_list[i])
+    print sql
+    try:
+        # Execute the SQL command
+        cursor.execute(sql)
+        db.commit()
+    except:
+        print "Error: unable to add entry!"
+
+
+
+db.close()
+print "Done!"
+
